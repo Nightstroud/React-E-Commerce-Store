daded1a934b56cd66a2f21ebd34fbef1a3f5770f
Nightstroud <Nightstroud@yahoo.com>
2020-01-12 14:23:36 -0700

    First Component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,12 @@
-console.log("It Works!");
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div><h1>This is the header to the file.</h1></div>
+        );
+    }
+}
+
+render(<App/>, window.document.getElementById('root'));
