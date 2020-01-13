bf2c84745893e3aab575b1b5c18fd1187a55524e
Nightstroud <Nightstroud@yahoo.com>
2020-01-12 22:45:05 -0700

    Finishing up with the setup

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Login from './components/login_page/login';
+import Home from './components/home_page/home';
+import Product from './components/product_details_page/product';
+import Cart from './components/cart_page/cart';
+import Header from './components/base/header';
+
+import { useSelector, useDispatch } from 'react-redux';
+
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+          <Route path={'/'} component={ Header } />
+            <Route path={'/login'} component={ Login } />
+            <Route path={'/home'} component={ Home } />
+            <Route path={'/products_page'} component={ Product } />
+            <Route path={'/cart'} component={ Cart } />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/cart_page/cart.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Cart extends React.Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <p>This is the cart page and it no longer has the crap in it.</p>
+                </div>
+
+            </div>
+        );
+    }
+}
